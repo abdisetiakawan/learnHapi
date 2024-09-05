1448f083a52240a8bde2b740232b50ba3fb63929
abdisetiakawan <bedikadiryt@gmail.com>
2024-09-05 20:26:05 +0700

    "Added new GET route for retrieving user by ID and updated existing GET route for retrieving all users"

--- src/controllers/users/users.controller.ts
+++ src/controllers/users/users.controller.ts
@@ -1,17 +1,23 @@
 import { Repository, DataSource } from "typeorm";
 import { UserEntity } from "../../db/entites";
-import {ResponseToolkit, ServerRoute, Request} from '@hapi/hapi'
+import { ResponseToolkit, ServerRoute, Request } from "@hapi/hapi";
 
 export const userController = (con: DataSource): Array<ServerRoute> => {
-    const userRepo: Repository<UserEntity> = con.getRepository(UserEntity);
-    return [
-        {
-            method: 'GET',
-            path: '/users',
-            handler: async (request: Request, h: ResponseToolkit) => {
-                const users = await userRepo.find();
-                return h.response(users).code(200);
-            }
-        }
-    ]
-}
+  const userRepo: Repository<UserEntity> = con.getRepository(UserEntity);
+  return [
+    {
+      method: "GET",
+      path: "/users",
+      handler: async (request: Request, h: ResponseToolkit, err?: Error) => {
+        const users = await userRepo.find();
+        return h.response(users).code(200);
+      },
+    },
+    {
+      method: "GET",
+      path: "/users/{id}", // GET /users/1
+      handler: ({ params: { id } }: Request, h: ResponseToolkit, err?: Error) =>
+        userRepo.findOne(id),
+    },
+  ];
+};
