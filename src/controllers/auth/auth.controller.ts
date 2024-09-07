c7ae762d2063323f27c8e9ca88459f450d886fab
abdisetiakawan <bedikadiryt@gmail.com>
2024-09-08 00:41:14 +0700

    "Updated auth controller to use Joi for validation and modified user entity creation"

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { DataSource, Repository } from "typeorm";
 import { UserEntity } from "../../db/entites";
 import { genSalt, hash } from "bcrypt";
 import { sign } from "jsonwebtoken";
-import { string, object, date } from "@hapi/joi";
+import * as Joi from "joi";
 import { ServerRoute, Request, ResponseToolkit } from "@hapi/hapi";
 
 export const authController = (con: DataSource): Array<ServerRoute> => {
@@ -14,8 +14,10 @@ export const authController = (con: DataSource): Array<ServerRoute> => {
       handler: async ({ payload }: Request, h: ResponseToolkit) => {
         const { firstName, lastName, email, password, birthOfDate, type } =
           payload as Partial<UserEntity>;
+
         const salt = await genSalt(10);
         const hashedPassword = await hash(password, salt);
+
         const user: Partial<UserEntity> = new UserEntity(
           firstName,
           lastName,
@@ -25,13 +27,44 @@ export const authController = (con: DataSource): Array<ServerRoute> => {
           hashedPassword,
           type
         );
+
         await userRepo.save<Partial<UserEntity>>(user);
         delete user.password;
         delete user.salt;
+
         return {
           ...user,
           accessToken: sign({ ...user }, "secretKeyharusnyadienv"),
         };
+      },
+      options: {
+        auth: false,
+        validate: {
+          payload: Joi.object({
+            firstName: Joi.string().required().min(4).max(255),
+            lastName: Joi.string().required().min(4).max(255),
+            email: Joi.string().email().required().email(),
+            password: Joi.string()
+              .pattern(
+                new RegExp(
+                  "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$"
+                )
+              )
+              .required()
+              .messages({
+                "string.pattern.base":
+                  "Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, a number, and a special character.",
+              }),
+            birthOfDate: Joi.date().optional().max("2010-01-01"),
+            type: Joi.string().required(),
+          }),
+          failAction(request: Request, h: ResponseToolkit, err: Error) {
+            throw err;
+          },
+          options: {
+            abortEarly: false,
+          },
+        },
       },
     },
   ];
